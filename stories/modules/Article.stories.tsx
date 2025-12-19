a27eba2bc1fcea6b7b1f67c22a7631065c3cfd14
Ava Collins <info@avacollins.dev>
2025-12-19 15:01:01 -0500

    chore: update packages

--- stories/modules/Article.stories.tsx
+++ stories/modules/Article.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 import { BackgroundImage } from '../components/BackgroundImage';
 import { YouTubeEmbed } from '../components/YouTubeEmbed';
 import { Article } from './Article';
--- stories/modules/Hero.stories.tsx
+++ stories/modules/Hero.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import type { Meta, StoryObj } from '@storybook/react';
-import { fn } from '@storybook/test';
+import type { Meta, StoryObj } from '@storybook/nextjs';
+import { fn } from 'storybook/test';
 
 import { Hero } from "../modules/Hero"
 
